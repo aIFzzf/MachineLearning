3f4cf5202bcf0fd2c7c2c47f8d620c5735a44b11
aIFzzf <845567595@qq.com>
2023-10-18 00:19:11 +0800

    add pca for test+

--- ColorPredict/PolynomialFeatures.py
+++ ColorPredict/PolynomialFeatures.py
@@ -9,6 +9,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.decomposition import PCA
 
 mpl.use('TkAgg')
 
@@ -37,53 +38,84 @@ def get_cv_score(degree):
     # 返回平均分数
     return np.mean(scores)
 
+# PCA算法
+def pca(data, n_components):
+    # 1. 数据标准化
+    mean = np.mean(data, axis=0)
+    std = np.std(data, axis=0)
+    data_normalized = (data - mean) / std
+
+    # 2. 计算协方差矩阵
+    covariance_matrix = np.cov(data_normalized.T)
+
+    # 3. 计算特征值和特征向量
+    eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
+
+    # 4. 选择主成分
+    idx = eigenvalues.argsort()[::-1]
+    eigenvectors = eigenvectors[:, idx]
+    eigenvectors = eigenvectors[:, :n_components]
+
+    # 5. 转换数据
+    transformed_data = data_normalized.dot(eigenvectors)
+
+    return transformed_data
+
+def testdegreee(max_degree):
+    best_degree = 1
+    best_score = -np.inf
+
+    for degree in range(1, max_degree + 1):
+        score = get_cv_score(degree)
+        if score > best_score:
+            best_score = score
+            best_degree = degree
+
+    print("best degree : ", best_degree)
+    return best_degree
+
+# ----- PCA -----
+# 降维到二维
+n_components = 3
+
+pca = PCA(n_components = n_components)
+X_pca = pca.fit_transform(X_train)
+print(X_pca)
+
+# ----- PCA -----
+
 max_degree = 10  # 设置要测试的最大多项式次数
-best_degree = 3
-# best_score = -np.inf
-#
-# for degree in range(1, max_degree + 1):
-#     score = get_cv_score(degree)
-#     if score > best_score:
-#         best_score = score
-#         best_degree = degree
-#
-# print("Best polynomial degree:", best_degree)
+# best_degree = 1
+best_degree = testdegreee(max_degree)
 
-
-X = np.hstack((color1_array, color2_array))
 y = blendcolor_array
 
 # 创建一个使用二次多项式特征的 Lasso 回归模型
-lasso_poly = make_pipeline(PolynomialFeatures(degree=best_degree), Lasso(alpha=0.5))
-
-# 训练模型
-lasso_poly.fit(X, y)
+# lasso_poly = make_pipeline(PolynomialFeatures(degree=best_degree), Lasso(alpha=0.1))
+#
+# # 训练模型
+# lasso_poly.fit(X_train, y)
 
 # 创建一个多项式回归模型
-# poly = PolynomialFeatures(degree=best_degree)
+poly = PolynomialFeatures(degree=best_degree)
 # X_train_poly = poly.fit_transform(X_train)
-# model = LinearRegression()
-# model.fit(X_train_poly, y_train)
+X_train_poly = poly.fit_transform(X_pca)
+reg = LinearRegression()
+reg.fit(X_train_poly, y)
 
 
 # 数据输入
 A_new = np.array([1, 1, 0])
 B_new = np.array([0, 0, 1])
 
-# lasso 回归
-input_data = np.hstack((A_new, B_new)).reshape(1, -1)
-predicted_middle_color = lasso_poly.predict(input_data)
-print("Predicted middle color: ", predicted_middle_color)
+X_new =  np.hstack((A_new, B_new)).reshape(1, -1)
 
+X_transformed_data = pca.transform(X_new)
 
-print("Coefficients: ", lasso_poly.named_steps['lasso'].coef_)
-# X_new = np.concatenate((A_new, B_new)).reshape(1, -1)
+X_new_poly = poly.transform(X_transformed_data)
 
-# 使用多项式特征生成器转换新的输入数据
-# X_new_poly = poly.transform(X_new)
-
-# C_new = model.predict(X_new_poly)
-# print("Predicted intermediate color:", C_new)
+C_new = reg.predict(X_new_poly)
+print("Predicted intermediate color:", C_new)
 
 # 获取多项式系数
 # coefs = model.coef_
